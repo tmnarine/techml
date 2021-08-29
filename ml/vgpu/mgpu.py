1d832a194ec05f5c7b7beb34f71c2e1f332cafd2
tmnarine <76846523+tmnarine@users.noreply.github.com>
2021-08-28 21:37:37 -0400

    Dev update (#5)
    * Minor updates.
    
    * Adding @kernel decorator to restrict global variable access.
    
    * Cleaned up compute shader code.
    
    * New compression example
    
    Cleaned up shader code in the virtual gpu sample.
    
    * New data format conversion example
    
    Converts NCHW to NHWC and back for verification.

--- ml/vgpu/mgpu.py
+++ ml/vgpu/mgpu.py
@@ -124,6 +124,82 @@ def test_x_times_x_plus_x_times_2():
     assert( cpu_result == buffer_result.contents() )
 
 
+def test_nchw_nhwc_conversion(N, C, H, W):
+    print("Test: %s" % vgpu.function_name())
+
+    # Device
+    device = vgpu.Device()
+    print("\tDevice: %s" % device.name())
+    
+    # Buffer
+    L = N * C * H * W
+    assert(L > 0)
+    buffer_nchw_in = device.newBufferWithSize(L)
+    buffer_nhwc_out = device.newBufferWithSize(L)
+    buffer_nchw_out = device.newBufferWithSize(L)
+
+    # Initialize buffer_in
+    for i in range(0,L):
+        buffer_nchw_in.set(i,i)
+
+    # Create kernel and pipeline state
+    kernelNCHW_2_NHWC = mcs.KernelNCHW_2_NHWC()
+    kernelNHWC_2_NCHW = mcs.KernelNHWC_2_NCHW()
+    kernel_NCHW_2_NHWC = vgpu.Kernel(kernelNCHW_2_NHWC.run)
+    pipeline_state_NCHW_2_NHWC = vgpu.PipelineState(kernel_NCHW_2_NHWC)
+    kernel_NHWC_2_NCHW = vgpu.Kernel(kernelNHWC_2_NCHW.run)
+    pipeline_state_NHWC_2_NCHW = vgpu.PipelineState(kernel_NHWC_2_NCHW)
+
+    # Dispatch thread setup
+    threadsPerThreadGroup = vgpu.Size(1, 1, 1)
+    threadsPerGrid = vgpu.Size(
+        N * C,
+        (H+threadsPerThreadGroup.x()-1)/threadsPerThreadGroup.x(),
+        (W+threadsPerThreadGroup.y()-1)/threadsPerThreadGroup.y() )
+   
+    cmd_buf = device.commandBuffer()
+    
+    encoder_nchw_nhwc = cmd_buf.computeCommandEncoder()
+    encoder_nchw_nhwc.setPipelinestate(pipeline_state_NCHW_2_NHWC)
+    
+    # Set the arguments
+    encoder_nchw_nhwc.addArg(buffer_nchw_in)
+    encoder_nchw_nhwc.addArg(buffer_nhwc_out)
+    encoder_nchw_nhwc.addArg(N)
+    encoder_nchw_nhwc.addArg(C)
+    encoder_nchw_nhwc.addArg(H)
+    encoder_nchw_nhwc.addArg(W)
+
+    encoder_nchw_nhwc.dispatchThreads(threadsPerGrid, threadsPerThreadGroup)
+
+    encoder_nhwc_nchw = cmd_buf.computeCommandEncoder()
+    encoder_nhwc_nchw.setPipelinestate(pipeline_state_NHWC_2_NCHW)
+
+    # Dispatch thread setup
+    threadsPerThreadGroup = vgpu.Size(1, 1, 1)
+    threadsPerGrid = vgpu.Size(
+        N * H,
+        (W+threadsPerThreadGroup.x()-1)/threadsPerThreadGroup.x(),
+        (C+threadsPerThreadGroup.y()-1)/threadsPerThreadGroup.y() )
+
+    # Set the arguments
+    encoder_nhwc_nchw.addArg(buffer_nhwc_out)
+    encoder_nhwc_nchw.addArg(buffer_nchw_out)
+    encoder_nhwc_nchw.addArg(N)
+    encoder_nhwc_nchw.addArg(C)
+    encoder_nhwc_nchw.addArg(H)
+    encoder_nhwc_nchw.addArg(W)
+
+    encoder_nhwc_nchw.dispatchThreads(threadsPerGrid, threadsPerThreadGroup) 
+
+    cmd_buf.submit()
+
+    if L > 1:
+        assert( buffer_nchw_in.notEquals(buffer_nhwc_out) )
+        assert( buffer_nhwc_out.notEquals(buffer_nchw_out) )
+    assert( buffer_nchw_in.equals(buffer_nchw_out) )
+
+
 #
 # Main
 #
@@ -136,6 +212,13 @@ def main():
 
     # Two kernels run together: result = x * x * 2
     test_x_times_x_plus_x_times_2()
+
+    # Run two kernels to perform data format conversion
+    # and verify the result
+    test_nchw_nhwc_conversion(1, 1, 1, 1)
+    test_nchw_nhwc_conversion(1, 3, 5, 6)
+    test_nchw_nhwc_conversion(1, 3, 6, 5)
+    test_nchw_nhwc_conversion(4, 6, 12, 12)
 
 if __name__ == "__main__":
     main()
