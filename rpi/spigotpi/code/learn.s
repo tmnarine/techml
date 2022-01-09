c9a3af39eafd7b1f5d91a311ea3c3fae4a406030
Tracy Narine <tmah32768@gmail.com>
2022-01-08 20:54:42 -0500

    Cleaning up return address handling.  Also minor documentation fixes.

--- rpi/spigotpi/code/learn.s
+++ rpi/spigotpi/code/learn.s
@@ -1,6 +1,6 @@
-@ Learning Rasberrypi assembler
+@ Learning Rasberry Pi assembler
 
-@ Rasberrypi assembler reference: 
+@ Rasberry Pi Assembler reference:
 @   https://personal.utdallas.edu/~pervin/RPiA/RPiA.pdf
 
 @ Data section - variables, strings etc
@@ -24,49 +24,45 @@ blankmsg: .asciz ""
 number: .word 32
 
 .balign 4
-main_return: .word 0
-
-.balign 4
-func_return: .word 0
-
-.balign 4
 Array: .skip 400
 
 @ Text section - code
 .text
 
+@ Functions have 3 parts:
+@ - Save the return address: push { lr }
+@ - body implementation
+@ - Restore the return address and branch to it: pop { lr } and bl lr
+
 @ r#i indicates an input
 @ r#o indicates and output
 
 print_str_r0i:
-    ldr r1, =func_return
-    str lr, [r1]
+    push { lr }
+    @ body
     bl puts
-    ldr r1, =func_return
-    ldr lr, [r1]
+    pop { lr }
     bx lr
     
 print_int_r0i_r1i:
-    ldr r2, =func_return
-    str lr, [r2]
+    push { lr }
+    @ body
     bl printf
-    ldr r2, =func_return
-    ldr lr, [r2]
+    pop { lr }
     bx lr
     
 print_int_cr_r0i_r1i:
-    ldr r2, =func_return
-    str lr, [r2]
+    push { lr }
+    @ body
     bl printf
     ldr r0, =blankmsg
     bl puts
-    ldr r2, =func_return
-    ldr lr, [r2]
+    pop { lr }
     bx lr
  
 print_int_array_r0i_r1i:
-    ldr r2, =func_return
-    str lr, [r2]
+    push { lr }
+    @ body
     mov r2, #0
     print_loop:
         cmp r2, r1
@@ -79,22 +75,20 @@ print_int_array_r0i_r1i:
         add r2, #1
         b print_loop
     end_print_loop:
-    ldr r2, =func_return
-    ldr lr, [r2]
+    pop { lr }
     bx lr
         
 multiply_by_10_r0i:
-    ldr r2, =func_return
-    str lr, [r2]
+    push { lr }
+    @ body
     add r1, r0, LSL #3 
     add r1, r0, LSL #1
-    ldr r2, =func_return
-    ldr lr, [r2]
+    pop { lr }
     bx lr
     
 divmod_r0i_r1i_r0o_r2o:
-    ldr r3, =func_return
-    str lr, [r3]
+    push { lr }
+    @ body
     mov r2, #0
     loop:
         cmp r0, r1
@@ -103,15 +97,15 @@ divmod_r0i_r1i_r0o_r2o:
         add r2, #1
         b loop
     end:
-    ldr r3, =func_return
-    ldr lr, [r3]
+    pop { lr }
     bx lr
     
 .global main
 main:
     @ Store the address we were called from
-    ldr r1, =main_return
-    str lr, [r1]
+    push { lr }
+
+    @ body implementation
     
     @ Print a string
     ldr r0, =saluation
@@ -119,7 +113,9 @@ main:
     
     @ Print a number and string
     ldr r0, =intfmt
+    @ Load the data address of number first
     ldr r1, =number
+    @ Then load the number from the data address
     ldr r1, [r1]
     bl print_int_cr_r0i_r1i
     
@@ -145,17 +141,19 @@ main:
     ldr r0, =intfmt
     bl print_int_cr_r0i_r1i
     
-    @ Set Array to all 2s
+    @ Set data Array to all 2s
     mov r0, #0
     ldr r1, =Array
     array_loop:
         cmp r0, #100
         bgt array_end
-        mov r2, #3
+        mov r2, #2
+        @ Address r1+4*r0 = r2
         str r2, [r1, +r0, LSL #2]
         add r0, #1
         b array_loop
     array_end:
+
     @ Set #0 to #1 to print array
     mov r0, #0
     cmp r0, #0
@@ -168,8 +166,7 @@ main:
     skip_print:
     
     @ Return to the address we were called from
-    ldr r1, =main_return
-    ldr lr, [r1]
+    pop { lr }
     bx lr
     
 
@@ -184,4 +181,6 @@ main:
 @ 1
 @ 5
 
+@ If you print the array you also get:
+@ 2,2,2,2,.....
 
