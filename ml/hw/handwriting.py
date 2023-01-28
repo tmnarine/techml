53ff50cabde58da4ec4676bcb93910120ba9c46c
tmnarine <tmah32768@gmail.com>
2023-01-27 22:18:36 -0500

    Rewrote parts of script
    - changed colour screen to match the mnist images so the prediction would work
    - using smoothscale so that shrinking the image samples local areas
    - tied the prediction into the title screen

--- ml/hw/handwriting.py
+++ ml/hw/handwriting.py
@@ -1,23 +1,107 @@
 # Simple canvas for entering numbers that will
 # be passed to Tensorflow for inference.
-#
-# Work in progress:
-# - Tensorflow/Keras hookup still to be done
+# Accurracy is low and seems to have trouble with
+# numbers like 9.  The drawing of the lines
+# is very simple and would need improvement
+# to fix this.
 #
 # Commands to setup:
-# python -m venv ./pygenv
+# python -m venv ./winenv
 # pip3 install pygame
 # pip3 install tensorflow
 # pip3 install keras
+# pip3 install matplotlib (used for debugging of images)
+#
+# Once environment is set  you can run with: python handwriting.py
+#
+# Lower case u will undo the last drawn line
 #
 
 import pygame
 
-use_tf = False
-if use_tf:
+try:
     import tensorflow as tf
-    from keras.datasets import minst
+    from keras import models
+    from keras import layers
+    from keras.utils import to_categorical
+    from keras.datasets import mnist
+    import numpy as np
+    import matplotlib.pyplot as plt
+    use_tf = True
+except:
+    print("# Tensorflow and keras.datasets not available")
+    use_tf = False
 
+#
+# Class for keras support of mnist
+#
+
+class MnistInference:
+
+    def __init__(self):
+        print("# Initializing tensorflow")
+
+        (self.train_images, self.train_labels), (self.test_images, self.test_labels) = mnist.load_data()
+
+        self.network = models.Sequential()
+        self.network.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
+        self.network.add(layers.Dense(10, activation='softmax'))
+
+        self.network.compile(optimizer='rmsprop',
+                            loss='categorical_crossentropy',
+                            metrics=['accuracy'])
+
+        self.train_images = self.train_images.reshape(60000, 28*28)
+        self.train_images = self.train_images.astype('float32') / 255
+        
+        self.test_images = self.test_images.reshape(10000, 28*28)
+        self.test_images = self.test_images.astype('float32') / 255
+
+        self.train_labels = to_categorical(self.train_labels)
+        self.test_labels = to_categorical(self.test_labels)
+
+        self.network.fit(self.train_images, self.train_labels, epochs=5, batch_size=128)
+
+        test_loss, test_accuracy = self.network.evaluate(self.test_images, self.test_labels)
+        print("# test_loss %g test_accuracy %g" % (test_loss, test_accuracy))
+
+        # Debugging
+        if False:
+            for idx in range(0,10):
+                print("# test label %s", self.train_labels[idx]);
+                print(self.train_images[idx])
+                self.displayImage(True, idx)
+       
+    def predictOneImage(self, nparray):
+        predictions = None
+        if self.network:
+            predictions = self.network.predict(nparray)
+        return predictions
+
+    def getImageAndLabel(self, test, idx):
+        label = self.test_labels[idx] if test else self.train_labels[idx]
+        image = self.test_images[idx] if test else self.train_images[idx]
+        return label, image
+
+    def displayImage(self, test, idx):
+        label, image = self.getImageAndLabel(test, idx)
+        #
+        im = self.train_images[idx]
+        im = im.reshape(28, 28)
+        plt.imshow(im, cmap=plt.cm.binary)
+        plt.show()
+       
+    def displayArrayImage(self, nparray, h=28, w=28):
+        im = nparray
+        im = im.reshape(h, w)
+        plt.imshow(im, cmap=plt.cm.binary)
+        plt.show()       
+
+
+#
+# Grid screen class to handle input and display
+# using pygame
+#
 
 class DrawGrid:
 
@@ -38,37 +122,39 @@ class DrawGrid:
         pygame.init()
 
         self.screen = None
-        self.input = [ "-", "-", "-", "-", "-", "-" ]
+        self.input = [ " - " for i in range(0,self.DIVISIONS*self.DIVISIONS) ]
         self.points = []
         self.lines = []
         self.lineUpdate = False
         self.drawing = False
         self.afterMouseUp = False
-        
-        if use_tf:
-            self.init_tf()
+        self.backgroundColour = self.BLACK
+        self.gridColour = self.GRAY
+        self.lineColour = self.GRAY
 
+        self.mnist = MnistInference()
+        
     def updateCaption(self):
         caption = self.TITLE + str(self.input)
         pygame.display.set_caption(caption)
         
     def run(self):
         caption = self.TITLE + str(self.input)
-        self.createWindow(caption, self.H, self.W, self.GRAY)
+        self.createWindow(caption, self.H, self.W, self.backgroundColour)
 
         running = True
         # Event loop
         while running:
             # Draw
             self.drawGrid( self.DIVISIONS, True )
-            # Extract images
-            if self.afterMouseUp and not self.drawing:
-                self.extractImages()
-                self.afterMouseUp = False
             #
             if self.lineUpdate:
                 self.drawLines(self.lines)
                 self.lineUpdate = False
+            # Extract images
+            if self.afterMouseUp and not self.drawing:
+                self.extractImages()
+                self.afterMouseUp = False
             # Event handling
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -78,8 +164,10 @@ class DrawGrid:
                         # Undo: control + u
                         if len(self.lines) > 0:
                             self.lines.pop()
-                            self.setBackground(self.GRAY)
+                            self.setBackground(self.backgroundColour)
                             self.lineUpdate = True
+                            # Allow undo operation to run character prediction
+                            self.afterMouseUp = True
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     self.points.append(event.pos)
                     self.drawing = True
@@ -96,37 +184,72 @@ class DrawGrid:
                         
         self.done()
 
-    def init_tf(self):
-        pass
-        
     def extractImages(self):
-        debug = True
+        debug = False
+        pygame.display.update()
         surface = pygame.display.get_surface().copy()
         subsurfaces = []
+        smallsufaces = []
         crects = []
-        views = []
+        # Reset the input string
+        self.input = []
         # Left, top, width, height
         rh = self.H/self.DIVISIONS
         wh = self.W/self.DIVISIONS
         for i in range(0, self.DIVISIONS):
             for j in range(0, self.DIVISIONS):
+                # Create the rectangle, position and inset it to avoid the grids
                 crect = pygame.Rect(0, 0, rh, wh)
-                crect.move_ip(i*rh, j*wh)
-                subsurface = surface.subsurface(crect)
-                small_surface =  pygame.transform.scale(subsurface, self.IMG_SIZE)
-                view = small_surface.get_view('3')
-                subsurfaces.append(subsurface)
-                crects.append(crect)
-                views.append(view)
+                crect.move_ip(j*rh, i*wh)
+                crect.inflate_ip(-8, -8)
+                # Get the subsurface copy
+                subsurface = pygame.display.get_surface().subsurface(crect).copy()
+                # Reduce the surface to the prediction image size we need
+                # Smooth scale is used as it averages pixels as it collapses the image
+                small_surface =  pygame.transform.smoothscale(subsurface, self.IMG_SIZE)
+                nprgb = pygame.surfarray.array_red(small_surface)
+                nprgb += pygame.surfarray.array_green(small_surface)
+                nprgb += pygame.surfarray.array_blue(small_surface)
+                nprgb = nprgb / 3.0
+                # Remove the transpose that pygame uses for draw data
+                nprgb = np.fliplr(nprgb)
+                nprgb = np.rot90(nprgb)
+                nprgb = nprgb.astype('float32') / 255.0
+                nprgb = nprgb.reshape(1, nprgb.shape[0]*nprgb.shape[1])
                 #
                 if debug:
+                    # nprgb = pygame.surfarray.array_red(subsurface) + pygame.surfarray.array_green(subsurface) + pygame.surfarray.array_blue(subsurface)
+                    # nprgb = nprgb / 3.0
+                    # nprgb = np.fliplr(nprgb)
+                    # nprgb = np.rot90(nprgb)
+                    # self.mnist.displayArrayImage(nprgb, 272, 272)
+                    self.mnist.displayArrayImage(nprgb)
+                    print(nprgb)
+                    subsurfaces.append(subsurface)
+                    smallsufaces.append(small_surface)
+                    crects.append(crect)
                     print("H %d W %d BYTES: %d" % (small_surface.get_height(), small_surface.get_width(), small_surface.get_bytesize()))
-                    print("\tview %d\n" % (view.length))
+                #
+                if self.mnist:
+                    predictions = self.mnist.predictOneImage(nprgb)
+                    # print("# prediction: ", predictions, predictions.shape)
+                    max_idx = -1
+                    maximum = 0
+                    for p in range(0, len(predictions[0])):
+                        if predictions[0,p] > maximum:
+                            max_idx = p
+                            maximum = predictions[0,p]
+                    if maximum > 0.4:
+                        # print("#### ",max_idx)
+                        self.input.append(" " + str(max_idx) + " ")
+                    else:
+                        self.input.append(" - ")
 
+        self.updateCaption()
+        
         if debug:
-            #pygame.display.get_surface().blit(subsurfaces[0], crects[-1])
-            pass
-    
+            pygame.display.get_surface().blit(smallsufaces[0], crects[-1])
+            
     def createWindow(self, name, h, w, color):
         self.screen = pygame.display.set_mode((h, w))
         self.setBackground(color)
@@ -144,7 +267,7 @@ class DrawGrid:
             for j in range(0, self.DIVISIONS):
                 crect = pygame.Rect(0, 0, rh, wh)
                 crect.move_ip(i*rh, j*wh)
-                pygame.draw.rect(self.screen, self.BLACK, crect, 3)
+                pygame.draw.rect(self.screen, self.gridColour, crect, 3)
         if update:
             self.updateCaption()
             pygame.display.update()
@@ -152,7 +275,8 @@ class DrawGrid:
     def drawLines(self, lines):
         for line in self.lines:
             # print(line)
-            pygame.draw.lines(self.screen, self.RED, False, line, 3)
+            width = 14
+            pygame.draw.lines(self.screen, self.lineColour, False, line, width)
        
     def done(self):
         pygame.quit()
